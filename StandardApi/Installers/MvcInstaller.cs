4ffb52ec21693b913e273390cfbead9929b5f73c
ZakariaOB <boukhriszakaria@gmail.com>
2021-04-17 16:56:09 +0000

    User Autentication with Jwt

--- StandardApi/Installers/MvcInstaller.cs
+++ StandardApi/Installers/MvcInstaller.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using StandardApi.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace StandardApi.Installers
 {
@@ -9,10 +15,49 @@ namespace StandardApi.Installers
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllersWithViews();
-            services.AddSwaggerGen(x =>
+            
+            var jwtSettings = new JwtSettings();
+            configuration.GetSection(nameof(JwtSettings)).Bind(jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            services.AddAuthentication(Options =>
             {
-                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Standard API", Version = "v1" });
+                Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x => 
+            {
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = false,
+                    ValidateLifetime = true
+                };
             });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Standard API", Version = "v1" });
+                
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", Array.Empty<string>() }
+                };
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme 
+                { 
+                    Description = "JWT Authorization header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+
         }
     }
 }
